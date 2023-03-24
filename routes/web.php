ea0870c72851647029301587c44a25c486c184f6
lukas-zx <lukas.koenn98@googlemail.com>
2023-03-24 09:56:58 +0100

    M1A6

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/testdata', [\App\Http\Controllers\AbTestDataController::class, 'getTestdata']);
+
+Route::get('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
+Route::get('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+Route::get('/isloggedin', [App\Http\Controllers\AuthController::class, 'isloggedin'])->name('haslogin');
